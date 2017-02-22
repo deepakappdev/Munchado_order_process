2715397a45d548989e951acc2b46354a13b32b78
deepakappdev <deepakappdev@gmail.com>
2017-02-22 14:21:03 +0530

    + initialize Volley

--- OrderProcessingApp/app/src/main/java/com/munchado/orderprocess/network/volley/RequestCallback.java
+++ OrderProcessingApp/app/src/main/java/com/munchado/orderprocess/network/volley/RequestCallback.java
@@ -3,7 +3,7 @@ package com.munchado.orderprocess.network.volley;
 import com.android.volley.NetworkError;
 
 public interface RequestCallback<T> {
-    public void error(NetworkError networkError);
+    void error(NetworkError networkError);
 
-    public void success(Object obj);
+    void success(Object obj);
 }
