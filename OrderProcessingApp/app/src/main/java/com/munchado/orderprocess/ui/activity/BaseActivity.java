0edb1adb0d97605c2057c203f22c95bed8852fe1
deepakappdev <deepakappdev@gmail.com>
2017-02-22 14:30:55 +0530

    + adding base files

---
+++ OrderProcessingApp/app/src/main/java/com/munchado/orderprocess/ui/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.munchado.orderprocess.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by android on 22/2/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
--- OrderProcessingApp/app/src/main/java/com/munchado/orderprocess/ui/activity/MainActivity.java
+++ OrderProcessingApp/app/src/main/java/com/munchado/orderprocess/ui/activity/MainActivity.java
@@ -1,10 +1,9 @@
 package com.munchado.orderprocess.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by android on 22/2/17.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 }
