0b7b38268228267d00840e13c90f32b4c4ba5dce
piyush <pkhare@munchado.in>
2017-02-23 13:54:18 +0530

    + commit

--- OrderProcessingApp/app/src/main/java/com/munchado/orderprocess/ui/activity/HomeActivity.java
+++ OrderProcessingApp/app/src/main/java/com/munchado/orderprocess/ui/activity/HomeActivity.java
@@ -10,8 +10,6 @@ import android.view.MenuItem;
 
 import com.munchado.orderprocess.R;
 import com.munchado.orderprocess.common.FRAGMENTS;
-import com.munchado.orderprocess.ui.fragment.ActiveOrderFragment;
-import com.munchado.orderprocess.ui.fragment.PrintSettingFragment;
 
 public class HomeActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -46,28 +44,6 @@ public class HomeActivity extends BaseActivity
                 super.onBackPressed();
         }
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.home, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
