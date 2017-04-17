4a1406ad95e4fde2c96c531846127c5b751d67bd
piyush <pkhare@munchado.in>
2017-04-17 16:37:16 +0530

    * Force update dialog is shown. * Downloading through download manager.

--- OrderProcessingApp/app/src/main/java/com/munchado/orderprocess/ui/activity/SplashActivity.java
+++ OrderProcessingApp/app/src/main/java/com/munchado/orderprocess/ui/activity/SplashActivity.java
@@ -1,11 +1,14 @@
 package com.munchado.orderprocess.ui.activity;
 
+import android.app.DownloadManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import com.munchado.orderprocess.R;
@@ -19,6 +22,9 @@ import com.munchado.orderprocess.utils.StringUtils;
 public class SplashActivity extends AppCompatActivity {
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 1500;
+    UpgradeData upgradeData;
+    private long enqueue;
+    private DownloadManager dm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +45,8 @@ public class SplashActivity extends AppCompatActivity {
 
         checkUpdate(versionCode + "");
 
-
+//        registerReceiver(receiver, new IntentFilter(
+//                DownloadManager.ACTION_DOWNLOAD_COMPLETE));
     }
 
     private void checkUpdate(String version) {
@@ -51,7 +58,7 @@ public class SplashActivity extends AppCompatActivity {
 
             @Override
             public void success(Object obj) {
-                UpgradeData upgradeData = (UpgradeData) obj;
+                upgradeData = (UpgradeData) obj;
                 if (upgradeData != null && upgradeData.data != null && !StringUtils.isNullOrEmpty(upgradeData.data.upgrade_type)) {
                     if (upgradeData.data.upgrade_type.equalsIgnoreCase("hard") || upgradeData.data.upgrade_type.equalsIgnoreCase("soft")) {
                         if (PrefUtil.getUpgradeType().isEmpty()) {
@@ -60,12 +67,13 @@ public class SplashActivity extends AppCompatActivity {
                         PrefUtil.setUpgradeClearData(upgradeData.data.clear_data);
                         PrefUtil.setUpgradeType(upgradeData.data.upgrade_type);
                         PrefUtil.setUpgradeMessage(upgradeData.data.message);
-
-                        if (upgradeData.data.upgrade_type.equalsIgnoreCase("hard") || (upgradeData.data.upgrade_type.equalsIgnoreCase("soft") && PrefUtil.getUpgradeDisplayCount() == 3)) {
-
-                            Intent i = new Intent(Intent.ACTION_VIEW);
-                            i.setData(Uri.parse(upgradeData.data.apk_link));
-                            startActivity(i);
+                        if (upgradeData.data.upgrade_type.equalsIgnoreCase("hard")) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+                            builder.setMessage("MA App Update Available.").setPositiveButton("Install", dialogClickListener).setCancelable(false).show();
+                        } else if ((upgradeData.data.upgrade_type.equalsIgnoreCase("soft") && PrefUtil.getUpgradeDisplayCount() == 3)) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+                            builder.setMessage("MA App Update Available.").setPositiveButton("Install", dialogClickListener)
+                                    .setNegativeButton("Cancel", dialogClickListener).show();
                         }
 
                     } else {
@@ -73,36 +81,83 @@ public class SplashActivity extends AppCompatActivity {
                         PrefUtil.setUpgradeType("");
                     }
 
-                    new Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
-                        @Override
-                        public void run() {
-                            // This method will be executed once the timer is over
-                            // Start your app main activity
-                            if (PrefUtil.isLogin()) {
-                                Intent i = new Intent(SplashActivity.this, HomeActivity.class);
-                                startActivity(i);
-                            } else {
-                                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
-                                startActivity(i);
-                            }
-
-
-                            // close this activity
-                            finish();
-                        }
-                    }, SPLASH_TIME_OUT);
                 }
             }
 
 
         });
     }
+
+    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    //Yes button clicked
+                    if (upgradeData != null) {
+//                        Intent i = new Intent(Intent.ACTION_VIEW);
+//                        i.setData(Uri.parse(upgradeData.data.apk_link));
+//                        startActivity(i);
+                        dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                        DownloadManager.Request request = new DownloadManager.Request(
+                                Uri.parse(upgradeData.data.apk_link));
+                        enqueue = dm.enqueue(request);
+                    }
+                    break;
+
+                case DialogInterface.BUTTON_NEGATIVE:
+                    //No button clicked
+                    break;
+            }
+            dialog.dismiss();
+            new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+                @Override
+                public void run() {
+                    // This method will be executed once the timer is over
+                    // Start your app main activity
+                    if (PrefUtil.isLogin()) {
+                        Intent i = new Intent(SplashActivity.this, HomeActivity.class);
+                        startActivity(i);
+                    } else {
+                        Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                        startActivity(i);
+                    }
+                    // close this activity
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }
+    };
+
+//    BroadcastReceiver receiver = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            String action = intent.getAction();
+//            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+//                long downloadId = intent.getLongExtra(
+//                        DownloadManager.EXTRA_DOWNLOAD_ID, 0);
+//                DownloadManager.Query query = new DownloadManager.Query();
+//                query.setFilterById(enqueue);
+//                Cursor c = dm.query(query);
+//                if (c.moveToFirst()) {
+//                    int columnIndex = c
+//                            .getColumnIndex(DownloadManager.COLUMN_STATUS);
+//                    if (DownloadManager.STATUS_SUCCESSFUL == c
+//                            .getInt(columnIndex)) {
+//
+//                        Toast.makeText(SplashActivity.this, "Updated apk has been downloaded successfully.", Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            }
+//        }
+//    };
 
     private void doWorkAfterUpdate() {
         if (PrefUtil.getUpgradeCleanData()) {
