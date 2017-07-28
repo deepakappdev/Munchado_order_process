cccd2403fc52ba354b1d07cfe501c414b9cb8aea
pranjulm3 <pmishra@bravvura.in>
2017-07-28 16:48:40 +0530

    + added reservation detail layout

--- OrderProcessingApp/app/src/main/java/com/munchado/orderprocess/ui/fragment/ReservationDetailFragment.java
+++ OrderProcessingApp/app/src/main/java/com/munchado/orderprocess/ui/fragment/ReservationDetailFragment.java
@@ -24,9 +24,11 @@ import com.munchado.orderprocess.network.RequestController;
 import com.munchado.orderprocess.network.volley.NetworkError;
 import com.munchado.orderprocess.network.volley.RequestCallback;
 import com.munchado.orderprocess.ui.activity.BaseActivity;
+import com.munchado.orderprocess.ui.widgets.CustomTextView;
 import com.munchado.orderprocess.utils.Constants;
 import com.munchado.orderprocess.utils.DateTimeUtils;
 import com.munchado.orderprocess.utils.StringUtils;
+import com.munchado.orderprocess.utils.Utils;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -41,24 +43,19 @@ public class ReservationDetailFragment extends BaseFragment implements View.OnCl
 
     String reservation_Id;
 
-    private TextView textName;
-    private TextView textEmail;
-    private TextView textTelephone;
-    private TextView textPastActivity;
-    private TextView textSubtotal;
-    private TextView textDealDiscount;
-    private TextView textPromocodeDiscount, textPromocetitle;
-    private TextView textDelivery, textDeliverytitle;
-    private TextView textTax;
-    private TextView textTip, textTiptitle;
-    private TextView textTotal;
+    private CustomTextView textName;
+    private CustomTextView textEmail;
+    private CustomTextView textTelephone;
+    private CustomTextView textPastActivity;
+    com.munchado.orderprocess.ui.widgets.SquareImageView iv_Profile;
+
     private LinearLayout orderLayout;
-    private TextView textOrderId;
-    private TextView text_receipt_no;
-    private TextView textOrderType;
-    private TextView textOrderTime;
-    private TextView labelOrderTime;
-    private TextView tv_change_delivery_time;
+    private CustomTextView text_booking_id;
+    private CustomTextView text_booking_time;
+    private CustomTextView text_no_people;
+    private CustomTextView text_time;
+    private CustomTextView text_status;
+    private CustomTextView text_instructions;
 
     private TextView textDeliveryTime;
     private TextView labelDeliveryAddress;
@@ -74,7 +71,7 @@ public class ReservationDetailFragment extends BaseFragment implements View.OnCl
     LinearLayout layout_profile, empty_layout;
     ImageButton layout_close;
     ImageButton btn_close;
-    RelativeLayout layout_base;
+//    RelativeLayout layout_base;
     private TextView textinstrctions;
     private NestedScrollView scrollView;
 
@@ -108,7 +105,7 @@ public class ReservationDetailFragment extends BaseFragment implements View.OnCl
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reservation_detail, container, false);
+        return rootView= inflater.inflate(R.layout.fragment_reservation_detail, container, false);
 
     }
     @Override
@@ -163,8 +160,6 @@ public class ReservationDetailFragment extends BaseFragment implements View.OnCl
         hideProgressBar();
         if (obj instanceof ReservationDetailsResponse) {
             response = (ReservationDetailsResponse) obj;
-          //  printData = ReceiptFormatUtils.setPrintData(response.data);
-//            LogUtils.e(printData);
             showDetail(response.data);
         }
 
@@ -172,26 +167,69 @@ public class ReservationDetailFragment extends BaseFragment implements View.OnCl
     }
 
     private void showDetail(ReservationDetailResponseData data) {
-        layout_profile = (LinearLayout) rootView.findViewById(R.id.layout_customer_detail_1);
-        empty_layout = (LinearLayout) rootView.findViewById(R.id.empty_layout);
+        layout_profile = (LinearLayout) rootView.findViewById(R.id.layout_customer_detail);
+//        empty_layout = (LinearLayout) rootView.findViewById(R.id.empty_layout);
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int height = displayMetrics.heightPixels;
         int width = displayMetrics.widthPixels;
         int w = (int) (width * .4);
         layout_profile.setLayoutParams(new RelativeLayout.LayoutParams(w, RelativeLayout.LayoutParams.MATCH_PARENT));
-        rootView.findViewById(R.id.layout_customer_detail_1).setVisibility(View.VISIBLE);
+        rootView.findViewById(R.id.ll_booking).setVisibility(View.VISIBLE);
+        textName = (CustomTextView) rootView.findViewById(R.id.text_name);
+        textEmail = (CustomTextView) rootView.findViewById(R.id.text_email);
+        textTelephone = (CustomTextView) rootView.findViewById(R.id.text_telephone);
+        textPastActivity = (CustomTextView) rootView.findViewById(R.id.text_past_activity);
+        iv_Profile = (com.munchado.orderprocess.ui.widgets.SquareImageView) rootView.findViewById(R.id.image_view);
+
         StringBuilder nameBuilder = new StringBuilder(data.getFirst_name());
         if (!StringUtils.isNullOrEmpty(data.getFirst_name()))
             nameBuilder.append(" ").append(data.getFirst_name());
         textName.setText(nameBuilder.toString());
         textTelephone.setText(data.getPhone());
         textEmail.setText(data.getEmail());
-        System.out.println("PHONENUMBER  "+data.getPhone());
-
-
+        layout_profile.setVisibility(View.VISIBLE);
+//        System.out.println("PHONENUMBER  "+data.getPhone());
+        StringBuilder pastActivity = new StringBuilder();
+        if (data.getPastActivity() != null) {
+            if (Integer.parseInt(data.getPastActivity().getTotalorder()) > 0)
+                pastActivity.append(data.getPastActivity().getTotalorder()).append(" Orders");
+            if (Integer.parseInt(data.getPastActivity().getTotalreservation() )> 0)
+                pastActivity.append(", ").append(data.getPastActivity().getTotalreservation()).append(" Reservation");
+            if (Integer.parseInt(data.getPastActivity().getTotalcheckin()) > 0)
+                pastActivity.append(", ").append(data.getPastActivity().getTotalcheckin()).append(" Checkins");
+            if (Integer.parseInt(data.getPastActivity().getTotalreview()) > 0)
+                pastActivity.append(", ").append(data.getPastActivity().getTotalreview()).append(" Reviews");
+        }
+        if (pastActivity.toString().isEmpty())
+            textPastActivity.setText("No Past Activity");
+        else
+            textPastActivity.setText(Utils.decodeHtml(pastActivity.toString()));
+        showOrderDetail(data);
     }
-        @Override
+
+    private void showOrderDetail(ReservationDetailResponseData orderDetailData) {
+        rootView.findViewById(R.id.ll_booking).setVisibility(View.VISIBLE);
+//        textOrderId.setText(orderDetailData.getReceipt_no());
+//        text_receipt_no.setText(orderDetailData.payment_receipt);
+//        textOrderType.setText(orderDetailData.order_type);
+
+        text_booking_id = (CustomTextView) rootView.findViewById(R.id.text_booking_id);
+        text_booking_time = (CustomTextView) rootView.findViewById(R.id.text_booking_time);
+        text_no_people = (CustomTextView) rootView.findViewById(R.id.text_no_people);
+        text_time = (CustomTextView) rootView.findViewById(R.id.text_time);
+        text_status = (CustomTextView) rootView.findViewById(R.id.text_status);
+        text_instructions = (CustomTextView) rootView.findViewById(R.id.text_instructions);
+
+        text_booking_id.setText(orderDetailData.getId());
+        text_booking_time.setText(orderDetailData.getReserved_on());
+        text_no_people.setText(orderDetailData.getParty_size()+" People");
+        text_time.setText(orderDetailData.getReservation_date_time());
+        text_status.setText(orderDetailData.getStatus());
+        text_instructions.setText(orderDetailData.getUser_instruction());
+    }
+
+    @Override
     public void onClick(View view) {
 
     }
